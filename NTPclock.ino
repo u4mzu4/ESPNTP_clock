4714b7847ce0d8e875b6abc07de8064dceb36f3e
andrew911 <39950568+u4mzu4@users.noreply.github.com>
2021-03-11 20:40:01 +0100

    Release 1.0

--- NTPclock.ino
+++ NTPclock.ino
@@ -4,6 +4,8 @@
 #include <credentials.h>
 
 // Defines
+#define BUT1        4
+#define BUT2        5
 #define CLK         14
 #define DIO         12
 #define NTPSERVER   "hu.pool.ntp.org"
@@ -16,8 +18,22 @@
 #define MAXREFRESH  59
 #define MAXMIN      59
 #define MAXHOUR     23
+#define MAXBYTE     254
 #define NIGHTBEFORE 7
 #define NIGHTAFTER  21
+#define SETUPDELAY  500
+
+
+//Enum
+enum SETUP_SM {
+  INIT				    = 0,
+  MAIN				    = 1,
+  WAITFORSETUP		= 2,
+  SETUP				    = 3,
+  WAITFORRESTART	= 4,
+  RESTART			    = 5,
+  RESERVED			  = 9,
+};
 
 
 //Global variables
@@ -29,6 +45,13 @@ const uint8_t SEG_FAIL[] = {
   SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G,   // A
   SEG_E | SEG_F,                                   // I
   SEG_D | SEG_E | SEG_F                            // L
+};
+
+const uint8_t SEG_SET[] = {
+  SEG_A | SEG_C | SEG_D | SEG_F | SEG_G,           // S
+  SEG_A | SEG_D | SEG_E | SEG_F | SEG_G,		       // E
+  SEG_D | SEG_E | SEG_F | SEG_G,                   // t
+  0
 };
 
 
@@ -92,23 +115,15 @@ void WiFi_Config()
   }
 }
 
-void WaitForMinute(unsigned long waitUntilNextMinute)
+void WaitForMinute(unsigned long waitfromPrevMinute)
 {
-  while (waitUntilNextMinute > millis())
+  while (millis() - waitfromPrevMinute < REFRESH )
   {
     delay(1000);
   }
   lastRefresh = millis();
   dateTime.minute++;
-  if (dateTime.minute > MAXMIN)
-  {
-    dateTime.minute = 0;
-    dateTime.hour++;
-    if (dateTime.hour > MAXHOUR)
-    {
-      dateTime.hour = 0;
-    }
-  }
+  TimeLimiter();
   RefreshDisplay(dateTime.hour, dateTime.minute);
 }
 
@@ -136,10 +151,188 @@ void AutoBrightness()
   }
 }
 
+void ButtonCheck()
+{
+  byte pressedButtons;
+  static bool firstPress = true;
+  static SETUP_SM setupState = INIT;
+  static SETUP_SM prevState = INIT;
+  static unsigned long stateEnterTime;
+  static unsigned long pressStartTime;
+  static byte setupStep = 1;
+
+  pressedButtons = ((!digitalRead(BUT1) << 1) | (!digitalRead(BUT2))); //3:BOTH, 2:BUT1, 1:BUT2, 0: NEITHER
+  
+  switch (setupState)
+  {
+    case INIT:
+      {
+        setupState = MAIN;
+        break;
+      }
+
+    case MAIN:
+      {
+        if (3 == pressedButtons)
+        {
+          stateEnterTime = millis();
+          setupState = WAITFORSETUP;
+        }
+        break;
+      }
+
+    case WAITFORSETUP:
+      {
+        if (pressedButtons < 3)
+        {
+          setupState = MAIN;
+          break;
+        }
+        else if (millis() - stateEnterTime > 3000)
+        {
+          dateTime.hour = 0;
+          dateTime.minute = 0;
+          display.setSegments(SEG_SET);
+          setupState = SETUP;
+          prevState = WAITFORSETUP;
+        }
+        else
+        {
+          break;
+        }
+        break;
+      }
+
+    case SETUP:
+      {
+        if (3 == pressedButtons)
+        {
+          if (WAITFORSETUP == prevState)
+          {
+            stateEnterTime = millis();
+            setupState = WAITFORRESTART;
+            break;
+          }
+          else
+          {
+            firstPress = true;
+            setupState = MAIN;
+            break;
+          }
+        }
+        if (1 == pressedButtons)
+        {
+          if (firstPress)
+          {
+            pressStartTime = millis();
+            firstPress = false;
+          }
+          setupStep = CalculateStep(pressStartTime);
+          dateTime.minute -= setupStep;
+          TimeLimiter();
+          RefreshDisplay(dateTime.hour, dateTime.minute);
+          lastRefresh = millis();
+          delay(SETUPDELAY);
+          break;
+        }
+        if (2 == pressedButtons)
+        {
+          if (firstPress)
+          {
+            pressStartTime = millis();
+            firstPress = false;
+          }
+          setupStep = CalculateStep(pressStartTime);
+          dateTime.minute += setupStep;
+          TimeLimiter();
+          RefreshDisplay(dateTime.hour, dateTime.minute);
+          lastRefresh = millis();
+          delay(SETUPDELAY);
+          break;
+        }
+        firstPress = true;
+        break;
+      }
+
+    case WAITFORRESTART:
+      {
+        if (pressedButtons < 3)
+        {
+          setupState = SETUP;
+          prevState = WAITFORRESTART;
+          break;
+        }
+        else if (millis() - stateEnterTime > 3000)
+        {
+          RefreshDisplay(88, 88);
+          setupState = RESTART;
+        }
+        else
+        {
+          break;
+        }
+        break;
+      }
+
+    case RESTART:
+      {
+        delay(1000);
+        ESP.restart();
+        break;
+      }
+  }
+}
+
+void TimeLimiter()
+{
+  if (dateTime.minute > MAXBYTE)
+  {
+    dateTime.minute = MAXMIN;
+    dateTime.hour--;
+    if (dateTime.hour > MAXBYTE)
+    {
+      dateTime.hour = MAXHOUR;
+    }
+  }
+  else if (dateTime.minute > MAXMIN)
+  {
+    dateTime.minute = 0;
+    dateTime.hour++;
+    if (dateTime.hour > MAXHOUR)
+    {
+      dateTime.hour = 0;
+    }
+  }
+  else
+  {
+    return;
+  }
+}
+
+byte CalculateStep(unsigned long pressStart)
+{
+  unsigned long buttonPressed;
+
+  buttonPressed = millis() - pressStart;
+
+  if (buttonPressed > 16 * SETUPDELAY)
+  {
+    return (60);
+  }
+  if (buttonPressed > 10 * SETUPDELAY)
+  {
+    return (10);
+  }
+  return (1);
+}
 
 void setup() {
   bool initRefresh = false;
-  unsigned long waitForExactMinute;
+  unsigned long prevMinute;
+
+  Serial.begin(115200);
+  pinMode(BUT1, INPUT_PULLUP);
+  pinMode(BUT2, INPUT_PULLUP);
 
   display.clear();
   display.setBrightness(BRIGHTNESS, true);
@@ -153,14 +346,16 @@ void setup() {
   {
     display.setSegments(SEG_FAIL);
   }
-  waitForExactMinute = millis() + REFRESH - dateTime.second * 1000;
-  WaitForMinute(waitForExactMinute);
+  prevMinute = millis() - dateTime.second * 1000;
+  WaitForMinute(prevMinute);
 }
 
 void loop() {
   bool validRefresh = false;
   byte storedHour;
-  unsigned long waitForNextMinute;
+  unsigned long prevMinute;
+
+  ButtonCheck();
 
   if ((millis() - lastRefresh) > REFRESH)
   {
@@ -181,8 +376,8 @@ void loop() {
       else
       {
         RefreshDisplay(dateTime.hour, dateTime.minute);
-        waitForNextMinute = millis() + REFRESH - dateTime.second * 1000;
-        WaitForMinute(waitForNextMinute);
+        prevMinute = millis() - dateTime.second * 1000;
+        WaitForMinute(prevMinute);
         return;
       }
     }
